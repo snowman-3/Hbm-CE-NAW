7959e97c145d98cbcd05c11eddff331a5fdd128f
mlbv <51232730+mlbv@users.noreply.github.com>
2026-01-04 20:08:51 +0800

    fix: oc pwr floppy disk, some advancement and qmaw

--- src/main/java/com/hbm/handler/CompatHandler.java
+++ src/main/java/com/hbm/handler/CompatHandler.java
@@ -2,10 +2,10 @@ package com.hbm.handler;
 
 import com.hbm.Tags;
 import com.hbm.blocks.ModBlocks;
-import com.hbm.inventory.RecipesCommon;
 import com.hbm.inventory.fluid.FluidType;
 import com.hbm.inventory.fluid.Fluids;
 import com.hbm.main.MainRegistry;
+import com.hbm.util.ItemStackUtil;
 import li.cil.oc.api.Items;
 import li.cil.oc.api.fs.FileSystem;
 import li.cil.oc.api.machine.Arguments;
@@ -18,7 +18,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
@@ -28,7 +27,6 @@ import java.util.concurrent.Callable;
 import static com.hbm.main.CraftingManager.addShapelessAuto;
 import static li.cil.oc.api.FileSystem.asReadOnly;
 import static li.cil.oc.api.FileSystem.fromClass;
-import static net.minecraft.item.EnumDyeColor.*;
 
 /**
  * General handler for OpenComputers compatibility.
@@ -42,15 +40,18 @@ public class CompatHandler {
      * @return Object[] array containing an int with the "compression level"
      */
     public static Object[] steamTypeToInt(FluidType type) {
-        return switch (type.getID()) {
-            case (4) -> // Fluids.HOTSTEAM
-                    new Object[]{1};
-            case (5) -> // Fluids.SUPERHOTSTEAM
-                    new Object[]{2};
-            case (6) -> // Fluids.ULTRAHOTSTEAM
-                    new Object[]{3};
-            default -> new Object[]{0};
-        };
+        if (type == null) {
+            return new Object[]{0};
+        }
+        int typeId = type.getID();
+        if (typeId == Fluids.HOTSTEAM.getID()) {
+            return new Object[]{1};
+        } else if (typeId == Fluids.SUPERHOTSTEAM.getID()) {
+            return new Object[]{2};
+        } else if (typeId == Fluids.ULTRAHOTSTEAM.getID()) {
+            return new Object[]{3};
+        }
+        return new Object[]{0};
     }
 
     /**
@@ -60,9 +61,9 @@ public class CompatHandler {
      */
     public static FluidType intToSteamType(int arg) {
         return switch (arg) {
-            case (1) -> Fluids.HOTSTEAM;
-            case (2) -> Fluids.SUPERHOTSTEAM;
-            case (3) -> Fluids.ULTRAHOTSTEAM;
+            case 1 -> Fluids.HOTSTEAM;
+            case 2 -> Fluids.SUPERHOTSTEAM;
+            case 3 -> Fluids.ULTRAHOTSTEAM;
             default -> Fluids.STEAM;
         };
     }
@@ -71,7 +72,7 @@ public class CompatHandler {
      * Allows for easy creation of read-only filesystems. Primarily for floppy disks.
      * (Though maybe reading directly from VOTV drives as filesystems could be implemented. :3)
      **/
-    private static class ReadOnlyFileSystem implements Callable<FileSystem> {
+    protected static class ReadOnlyFileSystem implements Callable<FileSystem> {
 
         private final String name;
 
@@ -81,7 +82,7 @@ public class CompatHandler {
 
         @Override
         @Optional.Method(modid = "opencomputers")
-        public li.cil.oc.api.fs.FileSystem call() throws Exception {
+        public FileSystem call() throws Exception {
             return asReadOnly(fromClass(MainRegistry.class, Tags.MODID, "disks/" + FloppyDisk.sanitizeName(name)));
         }
     }
@@ -89,7 +90,7 @@ public class CompatHandler {
     // Floppy disk class.
     public static class FloppyDisk {
         // Specifies the callable ReadOnlyFileSystem to allow OC to access the floppy.
-        public final ReadOnlyFileSystem fs;
+        protected final ReadOnlyFileSystem fs;
         // Specifies the color of the floppy disk (0-16 colors defined by OC).
         public final EnumDyeColor color;
         // Set after loading the disk; allows for adding a recipe to the item.
@@ -102,7 +103,7 @@ public class CompatHandler {
 
         // Disk names will be sanitized before the FileSystem is created.
         // This only affects the location/directory, not the display name.
-        // (Prevents filesystems from breaking/crashing due to having file separators, wildcards, etc.
+        // (Prevents filesystems from breaking/crashing due to having file separators, wildcards, etc.)
         public static String sanitizeName(String input) {
             return input.toLowerCase().replaceAll("\\W", "");
         }
@@ -111,24 +112,70 @@ public class CompatHandler {
     /**
      * Simple enum for mapping OC color ordinals to a nicer format for adding new disks.
      */
-    public static EnumDyeColor[] OCColors = new EnumDyeColor[] {
-        BLACK, //0x444444
-        RED, //0xB3312C
-        GREEN, //0x339911
-        BROWN, //0x51301A
-        BLUE, //0x6666FF
-        PURPLE, //0x7B2FBE
-        CYAN, //0x66FFFF
-        SILVER, //0xABABAB
-        GRAY, //0x666666
-        PINK, //0xD88198
-        LIME, //0x66FF66
-        YELLOW, //0xFFFF66
-        LIGHT_BLUE, //0xAAAAFF
-        MAGENTA, //0xC354CD
-        ORANGE, //0xEB8844
-        WHITE //0xF0F0F0
-    };
+    public enum OCColors {
+        BLACK(0x444444, "dyeBlack", EnumDyeColor.BLACK),
+        RED(0xB3312C, "dyeRed", EnumDyeColor.RED),
+        GREEN(0x339911, "dyeGreen", EnumDyeColor.GREEN),
+        BROWN(0x51301A, "dyeBrown", EnumDyeColor.BROWN),
+        BLUE(0x6666FF, "dyeBlue", EnumDyeColor.BLUE),
+        PURPLE(0x7B2FBE, "dyePurple", EnumDyeColor.PURPLE),
+        CYAN(0x66FFFF, "dyeCyan", EnumDyeColor.CYAN),
+        LIGHTGRAY(0xABABAB, "dyeLightGray", EnumDyeColor.SILVER),
+        GRAY(0x666666, "dyeGray", EnumDyeColor.GRAY),
+        PINK(0xD88198, "dyePink", EnumDyeColor.PINK),
+        LIME(0x66FF66, "dyeLime", EnumDyeColor.LIME),
+        YELLOW(0xFFFF66, "dyeYellow", EnumDyeColor.YELLOW),
+        LIGHTBLUE(0xAAAAFF, "dyeLightBlue", EnumDyeColor.LIGHT_BLUE),
+        MAGENTA(0xC354CD, "dyeMagenta", EnumDyeColor.MAGENTA),
+        ORANGE(0xEB8844, "dyeOrange", EnumDyeColor.ORANGE),
+        WHITE(0xF0F0F0, "dyeWhite", EnumDyeColor.WHITE),
+        NONE(0x0, "", EnumDyeColor.WHITE);
+
+        private final int color;
+        private final String dictName;
+        private final EnumDyeColor dyeColor;
+
+        OCColors(int color, String dictName, EnumDyeColor dyeColor) {
+            this.color = color;
+            this.dictName = dictName;
+            this.dyeColor = dyeColor;
+        }
+
+        public int getColor() {
+            return color;
+        }
+
+        public EnumDyeColor getDyeColor() {
+            return dyeColor;
+        }
+
+        // TODO: port BlockOpenComputersCablePaintable
+        public static OCColors fromInt(int intColor) {
+            for (OCColors iColor : OCColors.values()) {
+                if (intColor == iColor.getColor())
+                    return iColor;
+            }
+            return OCColors.NONE;
+        }
+
+        public static OCColors fromDye(ItemStack stack) {
+            List<String> oreNames = ItemStackUtil.getOreDictNames(stack);
+
+            for(String dict : oreNames) {
+                if(!(dict.length() > 3) || !dict.startsWith("dye"))
+                    continue;
+
+                for (OCColors color : OCColors.values()) {
+                    if(!color.dictName.equals(dict))
+                        continue;
+
+                    return color;
+                }
+            }
+
+            return OCColors.NONE;
+        }
+    }
 
     // Where all disks are stored with their name and `FloppyDisk` class.
     public static HashMap<String, FloppyDisk> disks = new HashMap<>();
@@ -146,13 +193,13 @@ public class CompatHandler {
             */
 
             // Idea/Code by instantnootles
-            disks.put("PWRangler", new FloppyDisk("PWRangler", OCColors[6]));
+            disks.put("PWRangler", new FloppyDisk("PWRangler", OCColors.CYAN.getDyeColor()));
 
             // begin registering disks
-            Logger logger = LogManager.getLogger("HBM");
+            Logger logger = MainRegistry.logger;
             logger.info("Loading OpenComputers disks...");
             if(disks.isEmpty()) {
-                logger.warn("No disks registered; see com.hbm.handler.CompatHandler.disks");
+                logger.info("No disks registered; see com.hbm.handler.CompatHandler.disks");
                 return;
             }
             disks.forEach((s, disk) -> {
@@ -167,25 +214,25 @@ public class CompatHandler {
 
                 } else { // Disk path DOES exist, and it should be loaded.
 
-                    disk.item = Items.registerFloppy(s, disk.color, disk.fs, true); // The big part, actually registering the floppies!
+                    disk.item = Items.registerFloppy(s, disk.color, disk.fs, false); // The big part, actually registering the floppies!
                     logger.info("Registered disk: {} at /assets/" + Tags.MODID + "/disks/{}", s, disk.fs.name);
 
                 }
             });
             logger.info("OpenComputers disks registered.");
-            logger.info("What we have: {}", disks.keySet());
-            // OC disk recipes!
-            List<ItemStack> floppyDisks = new RecipesCommon.OreDictStack("oc:floppy").toStacks();
 
-            if(!floppyDisks.isEmpty()) { //check that floppy disks even exist in oredict.
+            // OC disk recipes!
+//            List<ItemStack> floppyDisks = new RecipesCommon.OreDictStack("oc:floppy").toStacks();
+//
+//            if(!floppyDisks.isEmpty()) { //check that floppy disks even exist in oredict.
+
                 // Recipes must be initialized here, since if they were initialized in `CraftingManager` then the disk item would not be created yet.
-                // FIXME: This didn't add the correct recipes for the PWRangler disk.
-                addShapelessAuto(disks.get("PWRangler").item, new Object[] {"oc:floppy", new ItemStack(ModBlocks.block_bismuth)});
+                addShapelessAuto(disks.get("PWRangler").item, "oc:floppy", new ItemStack(ModBlocks.pwr_casing));
 
                 logger.info("OpenComputers disk recipe added for PWRangler.");
-            } else {
-                logger.info("OpenComputers floppy disk oredict not found, recipes cannot be loaded!");
-            }
+//            } else {
+//                logger.info("OpenComputers floppy disk oredict not found, recipes cannot be loaded!");
+//            }
 
             // boom, OC disks loaded
             logger.info("OpenComputers disks loaded.");
@@ -203,11 +250,9 @@ public class CompatHandler {
      * Pseudo multiblocks will automatically receive compatibility with their ports by proxying their `methods()` and `invoke()` functions. This is the only time they need to be defined.
      *
      **/
-    @Optional.InterfaceList({
-            @Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "opencomputers"),
-            @Optional.Interface(iface = "li.cil.oc.api.network.SidedComponent", modid = "opencomputers"),
-            @Optional.Interface(iface = "li.cil.oc.api.network.ManagedPeripheral", modid = "opencomputers"),
-    })
+    @Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "opencomputers")
+    @Optional.Interface(iface = "li.cil.oc.api.network.SidedComponent", modid = "opencomputers")
+    @Optional.Interface(iface = "li.cil.oc.api.network.ManagedPeripheral", modid = "opencomputers")
     @SimpleComponent.SkipInjection // make sure OC doesn't inject this shit into the interface and crash
     public interface OCComponent extends SimpleComponent, SidedComponent, ManagedPeripheral {
 
@@ -231,7 +276,7 @@ public class CompatHandler {
         @Override
         @Optional.Method(modid = "opencomputers")
         default boolean canConnectNode(EnumFacing side) {
-            return true;
+            return !this.getComponentName().equals(nullComponent);
         }
 
         /**
